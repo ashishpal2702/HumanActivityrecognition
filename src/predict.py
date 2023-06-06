d99349b186123184bd641a7f6d62ecc74ef7d846
Ashish Pal <ashishpal2702@gmail.com>
2023-06-06 18:15:25 +0530

    config update

--- src/predict.py
+++ src/predict.py
@@ -20,9 +20,9 @@ class Prediction:
 
     def build_test_feature(self, df):
         df = self.feat.preprocess(df)
-        cols = load_weights(self.config["feat_col"])
-        print(cols)
-        df = df[cols]
+        #cols = load_weights(self.config["feat_col"])
+        #print(cols)
+        #df = df[cols]
         df_transformed = self.feat.test_data_preprocessing_pipeline(df)
         print(df_transformed.shape)
         return df_transformed
@@ -41,8 +41,8 @@ class Prediction:
         test_df = self.md.make_test_dataset(df)
         x_test_std = self.build_test_feature(test_df)
         prediction = self.model_predict(x_test_std, threshold)
-        df["Churn_prediction"] = prediction
-        save_data(df, self.config["model_prediction"], update=True)
+        df["Activity"] = prediction
+        #save_data(df, self.config["model_prediction"], update=True)
         # Save daily model prediction
         dt_now = datetime.datetime.now(datetime.timezone.utc).strftime(
             "%Y-%m-%d_%H:%M:%S"
@@ -50,16 +50,21 @@ class Prediction:
         daily_model_prediction_file = (
             self.config["daily_prediction_dir"] + str(dt_now) + ".csv"
         )
-        save_data(df, daily_model_prediction_file, update=False)
+        #save_data(df, daily_model_prediction_file, update=False)
 
     def live_predict(self, df):
         threshold = 0.5
         x_test_std = self.build_test_feature(df)
-        prediction = self.model_predict(x_test_std, threshold)
-        return prediction
+        df['prediction'] = self.model_predict(x_test_std, threshold)
+
+        return df
 
 
 if __name__ == "__main__":
     update_log("Prediction customers who are likely to churn")
     config = load_config()
-    Prediction(config).bacth_predict()
+    #Prediction(config).bacth_predict()
+    test_df = pd.read_csv(config['testdata_file'])
+    prediction_table = Prediction(config).live_predict(test_df)
+
+    print(prediction_table.head())
